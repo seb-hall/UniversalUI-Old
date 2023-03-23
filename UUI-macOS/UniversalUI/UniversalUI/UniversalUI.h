8e55b432e2145c6cda7899dddfcc09d86bf8bd3f
sebhall <sebhalldesigns@gmail.com>
2023-03-23 10:57:37 +0000

    new file:   .DS_Store 	new file:   UUI-macOS/UniversalUI/UniversalUI.xcodeproj/project.pbxproj 	new file:   UUI-macOS/UniversalUI/UniversalUI.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   UUI-macOS/UniversalUI/UniversalUI.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   UUI-macOS/UniversalUI/UniversalUI.xcodeproj/project.xcworkspace/xcuserdata/seb.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   UUI-macOS/UniversalUI/UniversalUI.xcodeproj/xcuserdata/seb.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   UUI-macOS/UniversalUI/UniversalUI/UniversalUI.h 	new file:   UUI-macOS/UniversalUI/UniversalUI/UniversalUI.m 	new file:   UniversalUI/.DS_Store

---
+++ UUI-macOS/UniversalUI/UniversalUI/UniversalUI.h
@@ -0,0 +1,12 @@
+//
+//  UniversalUI.h
+//  UniversalUI
+//
+//  Created by Seb on 23/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UniversalUI : NSObject
+
+@end
---
+++ UUI-macOS/UniversalUI/UniversalUI/UniversalUI.m
@@ -0,0 +1,12 @@
+//
+//  UniversalUI.m
+//  UniversalUI
+//
+//  Created by Seb on 23/03/2023.
+//
+
+#import "UniversalUI.h"
+
+@implementation UniversalUI
+
+@end
